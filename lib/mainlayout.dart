f7e7cce6f0dee95ce67d3ea6763199bb67006535
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-04 02:39:25 +0700

    "Menambahkan constructor MainLayout dengan parameter child, title, showAppBar, dan actions."

--- lib/mainlayout.dart
+++ lib/mainlayout.dart
@@ -11,3 +11,16 @@ class MainLayout extends StatelessWidget {
   static const Color inputFillColor = Color(0xFFF8F9FA);
   static const Color inputBorderColor = Color(0xFFE0E0E0);
   static const Color labelColor = Color(0xFF4A4A44);
+
+  final Widget child;
+  final String title;
+  final bool showAppBar;
+  final List<Widget>? actions;
+
+  const MainLayout({
+    super.key,
+    required this.child,
+    this.title = '',
+    this.showAppBar = true,
+    this.actions,
+  });
