38f6d42c1e4d869489348f0868cd424353d60432
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-04 02:30:04 +0700

    "Menambahkan fitur logout dengan dialog konfirmasi."

--- lib/home.dart
+++ lib/home.dart
@@ -9,3 +9,48 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: "Home",
+      showAppBar: true,
+      actions: [
+        IconButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder:
+                  (context) => AlertDialog(
+                    title: Text("Logout"),
+                    content: Text("Are you sure you want to logout?"),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("Cencel"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamedAndRemoveUntil(
+                            context,
+                            '/login',
+                            (route) => false,
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.redAccent,
+                          foregroundColor: Colors.white,
+                        ),
+                        child: Text("Logout"),
+                      ),
+                    ],
+                  ),
+            );
+          },
+          icon: Icon(Icons.logout_rounded),
+          tooltip: 'Logout',
+        ),
+      ],
