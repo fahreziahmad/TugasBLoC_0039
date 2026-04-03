4484e0f334758e30a4b817f3288a7851098b9aad
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-04 02:21:05 +0700

    "Menambahkan struktur DetailOrderPage."

--- lib/detail_order.dart
+++ lib/detail_order.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tugas_bloc/home.dart';
 import 'package:tugas_bloc/mainlayout.dart';
+
+class DetailOrderPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final int jumlahMakanan;
+  final int jumlahMinuman;
+  final int totalHarga;
