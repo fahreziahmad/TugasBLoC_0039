4b1d92c76cd6dc9c407c296e7b6245f42b63727f
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-04 02:53:24 +0700

    - Menambahkan handler OrderSubmitted untuk menghitung total harga dan mengubah state menjadi OrderSuccess

--- lib/order_bloc.dart
+++ lib/order_bloc.dart
@@ -9,3 +9,31 @@ class OrderBloc extends Bloc<OrderEvent, OrderState> {
     on<OrderSubmitted>(_onOrderSubmitted);
     on<OrderReset>(_onOrderReset);
   }
+   Future<void> _onOrderSubmitted(
+    OrderSubmitted event,
+    Emitter<OrderState> emit,
+  ) async {
+    emit(OrderLoading());
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    final int totalHarga =
+        (event.jumlahMakanan * 32000) + (event.jumlahMinuman * 5000);
+
+    emit(
+      OrderSuccess(
+        makanan: event.makanan,
+        minuman: event.minuman,
+        jumlahMakanan: event.jumlahMakanan,
+        jumlahMinuman: event.jumlahMinuman,
+        totalHarga: totalHarga,
+      ),
+    );
+  }
+
+  void _onOrderReset(
+    OrderReset event,
+    Emitter<OrderState> emit,
+  ) {
+    emit(OrderInitial());
+  }
+}
