5dcac73f2dec49a39accfee97ad3986c1599f8f7
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-04 02:19:16 +0700

    "Menambahkan state AuthAuthenticated."

--- lib/auth_state.dart
+++ lib/auth_state.dart
@@ -7,3 +7,12 @@ abstract class AuthState extends Equatable {
 
 class AuthInitial extends AuthState {}
 class AuthLoading extends AuthState {}
+
+class AuthAuthenticated extends AuthState {
+  final String email;
+
+  AuthAuthenticated({required this.email});
+
+  @override
+  List<Object?> get props => [email];
+}
