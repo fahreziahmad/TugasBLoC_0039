5c2c2990eb9a17066dbc871ba7ba83fcb9d386ec
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-04 02:11:33 +0700

    "Menambahkan AuthBloc dengan handler login, register, dan logout.

--- lib/auth_bloc.dart
+++ lib/auth_bloc.dart
@@ -3,3 +3,10 @@ import 'package:equatable/equatable.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+	AuthBloc() : super(AuthInitial()) {
+		on<LoginSubmitted>(_onLoginSubmitted);
+		on<RegisterSubmitted>(_onRegisterSubmitted);
+		on<LogoutRequested>(_onLogoutRequested);
+	}
