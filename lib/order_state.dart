7bfa20143c5f4cff7473106771f581d418532bfe
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-04 02:55:52 +0700

    Tambah state OrderInitial, OrderLoading, OrderSuccess, OrderError di OrderBloc

---
+++ lib/order_state.dart
@@ -0,0 +1,52 @@
+part of 'order_bloc.dart';
+
+abstract class OrderState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class OrderInitial extends OrderState {}
+
+class OrderLoading extends OrderState {}
+
+class OrderSuccess extends OrderState {
+  final String makanan;
+  final String minuman;
+  final int jumlahMakanan;
+  final int jumlahMinuman;
+  final int totalHarga;
+
+  OrderSuccess({
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
+
+  Map<String, dynamic> toMap() => {
+    'makanan': makanan,
+    'minuman': minuman,
+    'jumlahMakanan': jumlahMakanan,
+    'jumlahMinuman': jumlahMinuman,
+    'totalHarga': totalHarga,
+  };
+
+  @override
+  List<Object?> get props => [
+    makanan,
+    minuman,
+    jumlahMakanan,
+    jumlahMinuman,
+    totalHarga,
+  ];
+}
+
+class OrderError extends OrderState {
+  final String message;
+
+  OrderError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
